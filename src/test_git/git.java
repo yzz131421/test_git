8cad20a4f676ff5fffbf5ee36f0e98339765bd75
yzz <209349050@qq.com>
2024-04-06 23:31:14 +0800

    Initial commit

---
+++ src/test_git/git.java
@@ -0,0 +1,9 @@
+package test_git;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("我是yzz");
+        System.out.println("我是yzz");
+        System.out.println("我是yzz");
+    }
+}
